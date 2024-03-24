674ef6b1080732bccbc45d7f139a2d564c11c06e
Abdul Rehman <154475349+AbdulRehman1632@users.noreply.github.com>
2024-03-24 16:36:14 -0700

    Add files via upload
    Excited to share my latest project - a responsive Tasbe counter website! Harnessing the power of technology to enhance spirituality and simplify daily rituals.

---
+++ app.js
@@ -0,0 +1,37 @@
+let text = document.getElementById("text");
+let count = 0;
+function Inc(){
+    count++;
+    text.innerText = count
+};
+
+function Dec(){
+    count=0;
+    text.innerText = count
+}
+
+
+
+let text2 = document.getElementById("text2");
+let count2 = 0;
+function Inc2(){
+    count2++;
+    text2.innerText = count2
+};
+
+function Dec2(){
+    count2=0;
+    text2.innerText = count2
+}
+
+let text3= document.getElementById("text3");
+let count3 = 0;
+function Inc3(){
+    count3++;
+    text3.innerText = count3
+};
+
+function Dec3(){
+    count3=0;
+    text3.innerText = count3
+}
